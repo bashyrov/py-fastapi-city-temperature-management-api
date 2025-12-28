3523b02bcfde918fc977fb1b0072061cc6c60166
Artur Bashyrov <bashyroov@gmail.com>
2025-12-28 22:09:33 +0100

    Update crud&list of temperatures

--- temperature/crud.py
+++ temperature/crud.py
@@ -7,9 +7,13 @@ def get_temperatures_list(db: Session, skip:int=0, limit: int=100):
     return db.query(Temperature).offset(skip).limit(limit).all()
 
 
-def create_temperatures(db: Session, temperature: schemas.TemperatureBase) -> Temperature:
+def create_temperatures(db: Session, temperature: schemas.TemperatureCreate) -> Temperature:
     db_temperature = Temperature(**temperature.model_dump())
     db.add(db_temperature)
     db.commit()
     db.refresh(db_temperature)
     return db_temperature
+
+def get_temperature_by_city_id(db: Session, city_id: int):
+    return db.get(Temperature.city_id, city_id)
+
--- temperature/router.py
+++ temperature/router.py
@@ -1,7 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+from sqlalchemy.orm import Session
+from dependencies import get_db
+from temperature.schemas import TemperatureBase, TemperatureRead, TemperatureCreate
+from temperature.crud import create_temperatures, get_temperatures_list, get_temperature_by_city_id
 
 router = APIRouter(prefix="/temperatures", tags=["temperatures"])
 
 @router.get("/")
-def read_temperatures():
-    return {"message": "List of temperatures"}
+def read_temperatures(db: Session = Depends(get_db)):
+    return get_temperatures_list(db)
+
+@router.delete("/{city_id}", response_model=TemperatureRead)
+def retrieve_temperature_for_single_city_endpoint(city_id: int, db: Session = Depends(get_db)):
+    result = get_temperature_by_city_id(db, city_id)
+    if not result:
+        raise HTTPException(
+            status_code=404,
+            detail="City not found"
+        )
+    return result
+
+
+@router.post("/create", response_model=TemperatureRead)
+def create_temperature_endpoint(temperature: TemperatureCreate, db: Session = Depends(get_db)):
+    return create_temperatures(db, temperature)
