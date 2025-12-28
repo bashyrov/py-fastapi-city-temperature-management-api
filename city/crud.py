00bef183be53b8a519b5962ad64624999fcb8133
Artur Bashyrov <bashyroov@gmail.com>
2025-12-28 20:39:42 +0100

    Added list and create function for City model

---
+++ city/crud.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+import schemas
+from city.models import City
+
+
+def get_cities_list(db: Session, skip:int=0, limit: int=100):
+    return db.query(City).offset(skip).limit(limit).all()
+
+
+def create_city(db: Session, city: schemas.CityCreate) -> City:
+    db_city = City(**city.model_dump())
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
